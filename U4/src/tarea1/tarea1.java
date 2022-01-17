adf6923a2f42adc5a656ddcdc4d3dc4e7a7a21ca
IsabelPastor <alum.ipastorl.2021@iesalixar.org>
2022-01-17 19:18:20 +0100

    Ejercicios en clase

--- U4/src/tarea1/tarea1.java
+++ U4/src/tarea1/tarea1.java
@@ -2,34 +2,41 @@ package tarea1;
 
 public class tarea1 {
     public static void main(String[] args) {
-        String nombre = "Pepita";
-        String dni = "55566222T";
-        CuentaCorriente cc = new CuentaCorriente(nombre, dni);
-        // comprobar atributos accesibles
-        System.out.println(cc.dni);
-        System.out.println(cc.nombre);
-        // no visibles
-        //System.out.println(cc.saldo);
-        //System.out.println(cc.limite);
+//        String nombre = "Pepita";
+//        String dni = "55566222T";
+//        CuentaCorriente cc = new CuentaCorriente(nombre, dni);
+//        // comprobar atributos accesibles
+//        System.out.println(cc.dni);
+//        System.out.println(cc.nombre);
+//        // no visibles
+//        //System.out.println(cc.saldo);
+//        //System.out.println(cc.limite);
 
-        int longitud = 13;
-        String frase = "ojÓ";
-        Texto txt = new Texto(longitud, frase);
-        System.out.println(txt.getCadena() + " " + txt.getCadena().length());
+//        int longitud = 13;
+//        String frase = "ojÓ";
+//        Texto txt = new Texto(longitud, frase);
+//        System.out.println(txt.getCadena() + " " + txt.getCadena().length());
+//
+//        txt.addCaracterFinal('b');
+//        txt.addCaracterPrincipio('C');
+//        txt.addStringPrincipio("aAAa");
+//        txt.addStringFinal("buib");
+//        System.out.println(txt.getCadena() + " " + txt.getCadena().length());
+//
+//        System.out.println(txt.contarVocalesMayusculas());
+//        System.out.println(txt.contarVocalesMinusculas());
 
-        txt.addCaracterFinal('b');
-        txt.addCaracterPrincipio('C');
-        txt.addStringPrincipio("aAAa");
-        txt.addStringFinal("buib");
-        System.out.println(txt.getCadena() + " " + txt.getCadena().length());
+//        Banco bnc = new Banco("Banco Alibaba","Avda. Italia");
+//        CuentaCorriente.cambiarBanco(bnc);
+//        cc.showInfo();
 
-        System.out.println(txt.contarVocalesMayusculas());
-        System.out.println(txt.contarVocalesMinusculas());
-
-        Banco bnc = new Banco("Banco Alibaba","Avda. Italia");
-        CuentaCorriente.cambiarBanco(bnc);
-        cc.showInfo();
-
-
+        SDEFM radio = new SDEFM();
+        System.out.println(radio.getDisplay());
+        radio.up();
+        System.out.println(radio.getDisplay());
+        radio.down();
+        System.out.println(radio.getDisplay());
+        radio.down();
+        System.out.println(radio.getDisplay());
     }
 }
