6442c633d3e61da923ac60b032c8c07126d31ff0
IsabelPastor <alum.ipastorl.2021@iesalixar.org>
2022-02-16 18:12:30 +0100

    Ejercicios clase

---
+++ U5/src/Tarea2/Cliente.java
@@ -0,0 +1,73 @@
+package Tarea2;
+
+import java.util.Comparator;
+
+public class Cliente implements Comparable {
+    private Integer dni;
+    private String nombre;
+    private Integer edad;
+    private Integer saldo;
+
+    /* ------- CONSTRUCTOR --------- */
+    public Cliente ( Integer dni, String nombre, Integer edad, Integer saldo){
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.saldo = saldo;
+    }
+
+
+    /* -------- MÉTODOS ------------- */
+    @Override
+    public boolean equals(Object objeto){
+        Cliente c = (Cliente) objeto;
+        return this.getDni().equals(c.getDni());
+    }
+
+    @Override
+    public String toString(){
+        String s = "DNI: " + getDni() + "\nNOMBRE: " + getNombre() + "\nEDAD: " + getEdad() + "\nSALDO: " + getSaldo();
+        return s;
+    }
+
+    /* --------- GETTER AND SETTER ---------- */
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Integer getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Integer saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Cliente c = (Cliente) o;
+        if (!(c instanceof Cliente))return 0;
+        return this.getDni()-c.getDni();
+    }
+}
--- U5/src/Tarea2/Tarea2.java
+++ U5/src/Tarea2/Tarea2.java
@@ -1,5 +1,7 @@
 package Tarea2;
 
+import java.util.Arrays;
+
 public class Tarea2 {
     public static void main(String[] args) {
       PilaTabla pl = new PilaTabla();
@@ -13,14 +15,24 @@ public class Tarea2 {
 //        pl.Desapilar();
 //        System.out.println(pl.toString());
 //        System.out.println(pl.getCima());
-        for (int i = 0; i < 10; i++) {
-            pl.Apilar(i);
+//        for (int i = 0; i < 10; i++) {
+//            pl.Apilar(i);
+//        }
+//        System.out.println(pl.toString());
+//        while (pl.getCima() != -1 ) {
+//            System.out.print(pl.getCima() + " ");
+//            pl.Desapilar();
+//        }
+//        System.out.println(pl.toString());
+        Cliente c1 = new Cliente(22124555, "Perico", 22, 2400);
+        Cliente c2 = new Cliente (21888555, "Pepa", 22, 2100);
+        Cliente c3 = new Cliente(21120110, "Elena", 37, 16000);
+        Cliente c4 = new Cliente(22084777, "Andres", 37, 11100);
+        Cliente [] clientes = {c1, c2, c3, c4};
+        Arrays.sort(clientes);
+        for (Cliente c: clientes
+             ) {
+            System.out.println(c.toString());
         }
-        System.out.println(pl.toString());
-        while (pl.getCima() != -1 ) {
-            System.out.print(pl.getCima() + " ");
-            pl.Desapilar();
-        }
-        System.out.println(pl.toString());
     }
 }
